0435c3c0380b43897bb2084f87ea306f7160c6a1
d903xb <stephen.day@dunelm.org.uk>
2014-12-06 13:04:59 +0000

    Initial commit

---
+++ TradeServer/src/main/java/activefx/tradserver/domain/Person.java
@@ -0,0 +1,13 @@
+package activefx.tradserver.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by steve on 06/12/2014.
+ */
+public class Person {
+
+    @Getter @Setter private String name;
+}
